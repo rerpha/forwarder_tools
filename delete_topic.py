951670c6fefe3ce03643b6ed15f90d2704cf34d5
Jack Harper <jack.harper@stfc.ac.uk>
2020-10-08 13:14:48 +0100

    add delete_topic.py

---
+++ delete_topic.py
@@ -0,0 +1,17 @@
+
+from confluent_kafka.admin import AdminClient
+import argparse
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="create kafka topic. Default partitions is 1"
+    )
+    parser.add_argument("-t", "--topic", help="the topic to create")
+    parser.add_argument("-b", "--broker", help="the broker to create the topics on")
+    args = parser.parse_args()
+
+    broker = args.broker
+    conf = {"bootstrap.servers": broker}
+    admin_client = AdminClient(conf)
+    admin_client.delete_topics([args.topic])
+    admin_client.poll(1)
